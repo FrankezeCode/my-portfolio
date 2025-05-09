3e91f6e226887fb1804dbb8130abbf783cd1c051
frankezecode <Franklynezeugonna@gmail.com>
2025-05-09 21:42:20 +0100

    updated my cv

--- data/projects.js
+++ data/projects.js
@@ -1,6 +1,6 @@
 export const listProjects = [
   {
-    image: "/projects/Eatopia.png",
+    image: "/projects/Eatopia.PNG",
     title: "Eatopia E-commerce",
     description:
       "This is a fullstack MERN (MongoDB, Express.js, React, Node.js) eCommerce web application that offers a rich online marketplace for food, fruits, vegetables, wine, and bakery products. It allows sellers to manage products and users to browse, add to cart, and place orders using cash on delivery or secure online payment through Stripe.",
@@ -8,7 +8,7 @@ export const listProjects = [
     github: "https://github.com/FrankezeCode/eatopia-fullstack"
   },
   {
-    image: "/projects/cardly.png",
+    image: "/projects/cardly.PNG",
     title: "Cardly GiftCard",
     description:
       "A fully responsive landing page of a GiftCard website from Figma Design, built and styled in 6 days with Next Js and TailwindCSS. Used GSAP library for animations.",
@@ -16,7 +16,7 @@ export const listProjects = [
     github: "https://github.com/FrankezeCode/Cardly"
   },
   {
-    image: "/projects/contrackr.png",
+    image: "/projects/contrackr.PNG",
     title: "Cointrackr",
     description:
       "A responsive crypto price aggregator website that fetches real-time data from the CoinGecko API. Built with React.js and styled using Tailwind CSS, it includes a Firebase backend with full user authentication and route protection.",
@@ -24,7 +24,7 @@ export const listProjects = [
     github: "https://github.com/FrankezeCode/Cointrackr"
   },
   {
-    image: "/projects/quicktask.png",
+    image: "/projects/quicktask.PNG",
     title: "QuickTask",
     description:
       "This app is called QuickTask. It functions as a project management tool that allows users to access a dashboard where they can create multiple projects. Each project can be viewed individually, and a series of tasks can be created with due dates, assigned to users, and monitored for progress.",
@@ -32,7 +32,7 @@ export const listProjects = [
     github: "https://github.com/FrankezeCode/QuickTask"
   },
   {
-    image: "/projects/codeai.png",
+    image: "/projects/codeai.PNG",
     title: "Coderverse AI",
     description:
       "A full-featured AI tool for generating images, videos, and text using OpenAI and Replicate APIs. Built with TypeScript, Clerk, Prisma, Tailwind CSS, and Radix UI, it offers a secure and responsive user experience.",
@@ -40,7 +40,7 @@ export const listProjects = [
     github: "https://github.com/FrankezeCode/coderverse-ai"
   },
   {
-    image: "/projects/virtualR.png",
+    image: "/projects/virtualR.PNG",
     title: "VirtualR",
     description:
       "A fully responsive landing page for a virtual reality website, built from a Figma design in 6 days using React.js and Tailwind CSS. The project features clean component structure, mobile-first design, and smooth UI implementation.",
